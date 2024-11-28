fff11781a60c5c0142d31688380a28342fc2b1a1
Datboi-Dan <datboidan961@gmail.com>
2024-11-27 23:10:48 -1000

    Update and rename Main.java to Scrambler.java

--- Scrambler.java
+++ Scrambler.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main
+public class Scrambler
 {
 
     public static void main(String[] args)
